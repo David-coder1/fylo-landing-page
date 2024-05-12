56380472c67d604db9fd3594d7c411003ce0ff48
Davidcoder <108403294+David-coder1@users.noreply.github.com>
2024-05-12 19:08:02 +0100

    commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("button");
+document.getElementById("secondButton")
+document.getElementById("firstTextDislay");
+document.getElementById("secondTetxDisplay")
+
+button.onclick = () => {
+    firstTextDislay.innerText += 'Please check your email '
+};
+
+
+secondButton.onclick = () => {
+    secondTextDisplay.innerText += "Please check your email"
+};
